5c64159b33b5ea666a8ab2dd1ba55000b7469dae
Rodrigojflima <drigo.a.lima@gmail.com>
2019-10-19 19:45:12 +0100

    Added dice similarity measure

--- mp1/similarity.py
+++ mp1/similarity.py
@@ -2,13 +2,25 @@ from nltk.metrics.distance import edit_distance
 from nltk.metrics.distance import jaccard_distance
 
 def jaccard(string1, string2):
+    '''
+    Jaccard distance
+    '''
     return jaccard_distance(
         set(string1.split()),
         set(string2.split())
     )
+
+def dice(string1, string2):
+    '''
+    Dice similarity measure
+    '''
+    return (2*(len(string1.intersection(string2)))) / (len(string1)+len(string2))
         
 
 def edit(string1, string2):
+    '''
+    Edit distance
+    '''
     return edit_distance(
         string1.split(),
         string2.split()
